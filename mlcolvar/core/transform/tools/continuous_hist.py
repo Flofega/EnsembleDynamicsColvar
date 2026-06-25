0b4f2e3ee29fb64f75c255c5e2f51e1dd01f11f6
Flofega <floriandietrich1998@gmail.com>
2026-06-25 14:44:25 +0200

    Added differentiable log histogram

--- mlcolvar/core/transform/tools/continuous_hist.py
+++ mlcolvar/core/transform/tools/continuous_hist.py
@@ -57,9 +57,69 @@ class ContinuousHistogram(Transform):
         x = self.compute_hist(x)
         return x
     
+
+class LogHistogram(Transform):
+    """
+    Compute continuous logarithmic histogram using Gaussian kernels
+    """
+
+    def __init__(self,
+                 in_features: int,
+                 min: float,
+                 max: float,
+                 bins: int,
+                 sigma_to_center: float = 1.0) -> torch.Tensor :
+        """Computes the continuous logarithmic histogram of a quantity using Gaussian kernels
+
+        Parameters
+        ----------
+        in_features : int
+            Number of inputs
+        min : float
+            Minimum value of the histogram
+        max : float
+            Maximum value of the histogram
+        bins : int
+            Number of bins of the histogram
+        sigma_to_center : float, optional
+            Sigma value in bin_size units, by default 1.0
+
+
+        Returns
+        -------
+        torch.Tensor
+            Values of the histogram for each bin
+        """
+
+        super().__init__(in_features=in_features, out_features=bins)
+
+        self.min = min
+        self.max = max
+        self.bins = bins
+        self.sigma_to_center = sigma_to_center
+
+    def compute_hist(self, x):
+        hist = easy_KDE(x=x,
+                        n_input=self.in_features,
+                        min_max=[self.min, self.max],
+                        n=self.bins,
+                        sigma_to_center=self.sigma_to_center)
+        return hist
+
+    def forward(self, x: torch.Tensor):
+        x = torch.log(self.compute_hist(x) + 1e-10) - -23.025850929940457  # add small value to avoid log(0)
+        return x
+    
 def test_continuous_histogram():
     x = torch.randn((5,100))
     x.requires_grad = True
     hist = ContinuousHistogram(in_features=100, min=-1, max=1, bins=10, sigma_to_center=1)
+    out = hist(x)
+    out.sum().backward()
+
+def test_log_histogram():
+    x = torch.randn((5,100))
+    x.requires_grad = True
+    hist = LogHistogram(in_features=100, min=-1, max=1, bins=10, sigma_to_center=1)
     out = hist(x)
     out.sum().backward()
